c8f9c85b8ae279d8558178f7ecb270d650478ea8
Roman Reutov <roman.reutov@wilix.org>
2022-10-19 15:49:39 +0300

    Telegram - Session recreating

--- telegram/src/main/kotlin/ru/vega/telegram/handler/update/MenuButtonCallbackUpdateHandler.kt
+++ telegram/src/main/kotlin/ru/vega/telegram/handler/update/MenuButtonCallbackUpdateHandler.kt
@@ -4,12 +4,15 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import dev.inmo.tgbotapi.extensions.utils.asCallbackQueryUpdate
 import dev.inmo.tgbotapi.extensions.utils.asMessageCallbackQuery
 import dev.inmo.tgbotapi.extensions.utils.extensions.raw.data
+import dev.inmo.tgbotapi.types.CallbackQuery.MessageCallbackQuery
 import dev.inmo.tgbotapi.types.buttons.Matrix
 import dev.inmo.tgbotapi.types.update.abstracts.Update
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import ru.vega.telegram.menu.Button
 import ru.vega.telegram.menu.ButtonId
+import ru.vega.telegram.menu.processor.StartMenuFactory
+import ru.vega.telegram.model.entity.Session
 import ru.vega.telegram.service.MenuService
 import ru.vega.telegram.service.SessionService
 
@@ -17,7 +20,7 @@ import ru.vega.telegram.service.SessionService
 class MenuButtonCallbackUpdateHandler(
     private val menuService: MenuService,
     private val sessionService: SessionService,
-    private val objectMapper: ObjectMapper
+    private val startMenuFactory: StartMenuFactory
 ) : UpdateHandler {
 
     companion object {
@@ -37,12 +40,12 @@ class MenuButtonCallbackUpdateHandler(
             return
         }
 
-        logger.info("New button callback {}", buttonId)
+        // TODO Пренести в другой хендлер обновления сессий?
 
-        val session = sessionService.getSession(query.message) ?: run {
-            logger.warn("Query update {} is out of session", query)
-            return
-        }
+        val session = sessionService.getSession(query.message) ?:
+            handleOutOfSession(query)
+
+        logger.debug("Pressed button with id {} by user {}", buttonId, session.user)
 
         val menu = session.menuHistory.currentMenu ?: run {
             logger.error("Session for message ${session.messageIdentifier} from ${session.user} dont have current menu")
@@ -61,6 +64,16 @@ class MenuButtonCallbackUpdateHandler(
         if (menuAfterCallback != null) {
             menuService.replaceMenu(query.message.chat.id, query.message.messageId, menuAfterCallback)
         }
+    }
+
+    private suspend fun handleOutOfSession(query: MessageCallbackQuery): Session {
+        logger.info("Message out of session. Recreating session and return session state to start menu.")
+
+        val startMenu = startMenuFactory.create()
+        val menuMessage = menuService.replaceMenu(query.message.chat.id, query.message.messageId, startMenu)
+        val session = sessionService.startSession(menuMessage)
+        session.menuHistory.pushNextMenu(startMenu)
+        return session
     }
 
     private fun findButton(keyboard: Matrix<Button>, buttonId: ButtonId): Button? =
