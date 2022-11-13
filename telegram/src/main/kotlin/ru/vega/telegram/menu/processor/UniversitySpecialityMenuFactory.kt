842c4c6930ba0d1cfe8fdf2827586c1ce738a993
Roman Reutov <peytob23@gmail.com>
2022-11-13 14:11:33 +0300

    Telegram - Fixes

--- telegram/src/main/kotlin/ru/vega/telegram/menu/processor/UniversitySpecialityMenuFactory.kt
+++ telegram/src/main/kotlin/ru/vega/telegram/menu/processor/UniversitySpecialityMenuFactory.kt
@@ -2,7 +2,9 @@ package ru.vega.telegram.menu.processor
 
 import dev.inmo.tgbotapi.utils.matrix
 import dev.inmo.tgbotapi.utils.row
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import ru.vega.model.dto.university.UniversityDto
 import ru.vega.model.dto.university.UniversitySpecialityDto
 import ru.vega.telegram.exception.EntityNotFound
 import ru.vega.telegram.menu.Button
@@ -12,20 +14,33 @@ import java.util.*
 
 @Component
 class UniversitySpecialityMenuFactory(
-    private val universitySpecialityService: UniversitySpecialityService
+    private val universitySpecialityService: UniversitySpecialityService,
 ) : MenuFactory {
+
+    // Resolution of circular reference.
+    @set:Autowired
+    var universityDetailsMenuFactory: UniversityDetailsMenuFactory? = null
 
     fun create(universitySpecialityId: UUID): Menu {
         val specialityDto = universitySpecialityService.getById(universitySpecialityId)
             ?: throw EntityNotFound("University speciality with ID $universitySpecialityId not found")
 
         val buttons = matrix<Button> {
+            row(makeUniversityButton(specialityDto.university))
             row(makeReturnButton())
         }
 
         val message = makeMessage(specialityDto)
 
         return Menu(buttons, message)
+    }
+
+    private fun makeUniversityButton(university: UniversityDto) = Button(university.title,  uuidAsByteString(university.id)) { session ->
+        universityDetailsMenuFactory
+            ?.create(university.id)
+            ?.let {
+                session.menuHistory.pushNextMenu(it)
+            }
     }
 
     private fun makeMessage(speciality: UniversitySpecialityDto): String {
